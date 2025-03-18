e9f0aa463c711dd4823a4da5332f56e7bc9d12d9
AdamPotato <namelessbtw123@gmail.com>
2025-03-18 13:06:38 +0000

    fix v2

--- JokerScript.js
+++ JokerScript.js
@@ -1,6 +1,6 @@
 function Changecolour () {
     const colour = ["#FF0000","#FFFF00","#00FF00","#00FFFF","#0000FF","#FF00FF","#9000ad"];
-    const Randomcolour = colour[Math.floor(Math.random() * colour.length)]
+    const Randomcolour = colour[Math.floor(Math.random() * colour.length)];
 
     document.body.style.backgroundColor = Randomcolour;
 }
